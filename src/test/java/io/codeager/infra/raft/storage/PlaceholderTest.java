660de8bccf3d9c32d3d106e57e4cc7604228f374
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2019-04-24 16:38:10 -0400

    add test placeholders

---
+++ src/test/java/io/codeager/infra/raft/storage/PlaceholderTest.java
@@ -0,0 +1,14 @@
+package io.codeager.infra.raft.storage;
+
+import org.junit.Test;
+
+/**
+ * @author Jiupeng Zhang
+ * @since 04/24/2019
+ */
+public class PlaceholderTest {
+    @Test
+    public void demo() {
+        System.out.println("hello");
+    }
+}
