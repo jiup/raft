e58bfbd4a17254966c37693209abb92b2f647e20
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2019-04-26 20:16:59 -0400

    add repeated_timer

---
+++ src/test/java/io/codeager/infra/raft/util/timer/RepeatedTimerTest.java
@@ -0,0 +1,42 @@
+package io.codeager.infra.raft.util.timer;
+
+import org.junit.Test;
+
+/**
+ * @author Jiupeng Zhang
+ * @since 04/26/2019
+ */
+public class RepeatedTimerTest {
+    @Test
+    public void test() {
+        RepeatedTimer timer = new RepeatedTimer("hello world", 1000) {
+            @Override
+            protected void onTrigger() {
+                System.out.println("hello");
+            }
+        };
+        timer.start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        RepeatedTimer timer = new RepeatedTimer("hello world", 1000) {
+            @Override
+            protected void onTrigger() {
+                System.out.println("hello");
+            }
+
+            @Override
+            protected void onDestroy() {
+                System.out.println("done!");
+            }
+        };
+        timer.start();
+        Thread.sleep(3000);
+        timer.stop();
+        Thread.sleep(3000);
+        timer.start();
+        Thread.sleep(3000);
+        timer.destroy();
+        System.out.println(timer.getTriggerCount());
+    }
+}
