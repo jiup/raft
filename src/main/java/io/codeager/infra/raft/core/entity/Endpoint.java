8f19ace02f8b9fd5f2da0c291f71fe975cd52855
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2019-04-30 20:50:15 -0400

    update endpoint entity: change method name update configuration entity: fit module design

--- src/main/java/io/codeager/infra/raft/core/entity/Endpoint.java
+++ src/main/java/io/codeager/infra/raft/core/entity/Endpoint.java
@@ -1,5 +1,7 @@
 package io.codeager.infra.raft.core.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
@@ -8,37 +10,43 @@ import org.apache.commons.lang.builder.ToStringStyle;
  * @since 04/29/2019
  */
 public final class Endpoint {
-    public static final Endpoint DEFAULT = Endpoint.from("0.0.0.0", 9990);
+    public static final Endpoint DEFAULT = Endpoint.of("0.0.0.0", 9990);
 
     private String host;
     private int port;
 
-    private Endpoint(String host, int port) {
+    @JsonCreator
+    private Endpoint(
+            @JsonProperty("host") String host,
+            @JsonProperty("port") int port) {
         this.host = host;
         this.port = port;
     }
 
-    public static Endpoint from(String address) {
+    @JsonCreator
+    private Endpoint(String address) {
         try {
             int separatorIndex = address.lastIndexOf(':');
-            return from(
-                    separatorIndex == 0 ? "0.0.0.0" : address.substring(0, separatorIndex),
-                    Integer.parseInt(address.substring(separatorIndex + 1))
-            );
+            this.host = separatorIndex == 0 ? "0.0.0.0" : address.substring(0, separatorIndex);
+            this.port = Integer.parseInt(address.substring(separatorIndex + 1));
         } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid endpoint address (" + e.getMessage() + ")");
+            throw new IllegalArgumentException("invalid endpoint address (" + e.getMessage() + ")");
         }
     }
 
-    public static Endpoint from(int port) {
-        return from("0.0.0.0", port);
+    public static Endpoint of(int port) {
+        return of("0.0.0.0", port);
     }
 
-    public static Endpoint from(String host, int port) {
+    public static Endpoint of(String host, int port) {
         if (port < 1 || port > 65535)
-            throw new IllegalArgumentException("port must range from 1 to 65535");
+            throw new IllegalArgumentException("port must range of 1 to 65535");
 
         return new Endpoint(host, port);
+    }
+
+    public static Endpoint of(String address) {
+        return new Endpoint(address);
     }
 
     public String getHost() {
