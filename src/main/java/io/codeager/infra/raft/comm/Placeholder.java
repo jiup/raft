0cce4a080b0b475210c88fe8f4d3a903de413caf
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2019-05-03 11:26:39 -0400

    remove package-comm

--- src/main/java/io/codeager/infra/raft/comm/Placeholder.java
+++
@@ -1,8 +0,0 @@
-package io.codeager.infra.raft.comm;
-
-/**
- * @author Jiupeng Zhang
- * @since 04/24/2019
- */
-public class Placeholder {
-}
---
+++ src/test/java/io/codeager/infra/raft/cli/PlaceHolder.java
@@ -0,0 +1,8 @@
+package io.codeager.infra.raft.cli;
+
+/**
+ * @author Jiupeng Zhang
+ * @since 05/03/2019
+ */
+public class PlaceHolder {
+}
